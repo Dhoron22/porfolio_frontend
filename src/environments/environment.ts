3fabef52a68a5e3c70fb0fe6bf1ec774fb55527c
Lautaro <perezlautaro63@gmail.com>
2026-03-14 17:37:00 -0300

    Update API URL to Render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://porfolio-backend.onrender.com/api'
+  apiUrl: 'https://porfolio-backend-dx5i.onrender.com/api'
 
  // https://porfoliobackend-production-02b6.up.railway.app/api
 };
